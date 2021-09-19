b1988f4be6006908a233caffb97459da9fd5e40d
Samuel Almeida <samuel@bravion.dev>
2021-09-19 11:41:03 -0300

    Added MONGO_URI env variable

--- database/index.js
+++ database/index.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 const connectToDatabase = async () => {
   try {
-    const mongo_uri = `mongodb://${process.env.DB_USERNAME}:${encodeURIComponent(process.env.DB_PASSWORD)}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+    if (process.env.MONGO_URI) {
+      var mongo_uri = process.env.MONGO_URI
+    } else {
+      var mongo_uri = `mongodb://${process.env.DB_USERNAME}:${encodeURIComponent(process.env.DB_PASSWORD)}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+    }
     const connection = await mongoose.connect(mongo_uri)
 
     console.log(`Connected to database ${connection.connection.name} at ${connection.connection.host}:${connection.connection.port}`)
